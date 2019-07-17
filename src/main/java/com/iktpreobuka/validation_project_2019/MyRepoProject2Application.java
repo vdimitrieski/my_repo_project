987535f00674f71db3a22d7dbed9b9a2245e0336
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2019-07-17 20:57:58 +0200

    my first commit

---
+++ src/main/java/com/iktpreobuka/validation_project_2019/MyRepoProject2Application.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.validation_project_2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepoProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRepoProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iktpreobuka/validation_project_2019/MyRepoProject2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iktpreobuka.validation_project_2019;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRepoProject2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
